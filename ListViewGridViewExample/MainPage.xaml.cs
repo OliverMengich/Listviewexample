d7fc68d5f7c74be1879d44ebc5f62e8b6cd60d4c
Oliver.Mengich <kemeioliver2000@gmail.com>
2021-01-18 09:17:45 +0300

    ListViewexample

---
+++ ListViewGridViewExample/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using ListViewGridViewExample.Models;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ListViewGridViewExample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Contact> Con;
+
+        // This method should be defined within your main page class.
+        
+        public MainPage()
+        {
+            Contact c = new Contact();
+            this.InitializeComponent();
+            Con = c.Manage();
+        }
+    }
+    
+}
---
+++ ListViewGridViewExample/Models/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewGridViewExample.Models
+{
+    
+    class Contact
+    {
+        public ObservableCollection<Contact> Contacts = new ObservableCollection<Contact>();
+
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string COmpany { get; set; }
+        public ObservableCollection<Contact> Manage()
+        {
+
+            Contacts.Add(new Contact { FirstName = "Oliver ", LastName = "Kipkemei ", COmpany = "Zee Music " });
+            Contacts.Add(new Contact { FirstName = "Thiago ", LastName = "Silva ", COmpany = "Chelsea" });
+            Contacts.Add(new Contact { FirstName = "Gabriel ", LastName = "Maghalhes ", COmpany = "Arsenal " });
+            Contacts.Add(new Contact { FirstName = "Kieran", LastName = "Tierney ", COmpany = "Arseanal " });
+
+            return Contacts;
+        }
+    }
+    
+}
